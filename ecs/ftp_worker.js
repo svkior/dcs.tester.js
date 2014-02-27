5c365211e7d4a789688bca8551cec8171ea877f1
Sergey V. Kior <svkior@gmail.com>
2014-02-27 16:46:57 +0400

    Comment encoder not works

--- ecs/ftp_worker.js
+++ ecs/ftp_worker.js
@@ -39,26 +39,26 @@ eventBus.registerHandler('ftp.load', function(args, responder){
     var vals = ftpTelemetry.getSensVal();
     var trajv = ftpTelemetry.getTrgenVal();
     var ustv   = ftpTelemetry.getRegVal();
-    var encoder = ftpTelemetry.getEncoder();
+   // var encoder = ftpTelemetry.getEncoder();
 
 
     var telem = [];
     var traj = [];
     var ust = [];
-    var enc = [];
+    //var enc = [];
     for(var i=0; i<vals.size(); i++){
         telem[i] = vals.get(i);
         traj[i] = trajv.get(i);
         ust[i] = ustv.get(i);
-        enc[i] = enc.get(i);
+       // enc[i] = enc.get(i);
     }
 
     responder({
         status: 'ok',
         data: telem,
         traj: traj,
-        ust: ust,
-        enc: enc
+        ust: ust
+        //enc: enc
     });
 
 });
